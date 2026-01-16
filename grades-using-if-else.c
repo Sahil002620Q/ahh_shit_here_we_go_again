d4875b084315b5cc63a0e1a38c87b99572b516cc
Sahil002620Q <smax06388@gmail.com>
2026-01-16 17:14:45 +0530

    grades

--- grades-using-if-else.c
+++ grades-using-if-else.c
@@ -1,24 +1,11 @@
 #include <stdio.h> 
 int main () {
-     int x;
-     printf("\nenter grade : \n");
-     scanf("%d",&x);
-     printf("you got ");
-     if(x<=30){
-        printf("fail");
-     }else if(x<=70){
-        printf(" B grade");
-     }else if(x<=80){
-        printf(" B+ grade");
-     }else if(x<=90){
-        printf(" A grade");
-     }else if(x<=100){
-        printf(" A+ grade");
-     }else{
-        printf("an error ");
+    int coutd = 10;
+     while (coutd >= 0)
+     {
+         printf("%d\n",coutd);
+         coutd--;
      }
-     
-  //ladder if else
 
 
 }
---
+++ while-practice.c
@@ -0,0 +1,3 @@
+//
+// Created by sahil on 1/16/2026.
+//
