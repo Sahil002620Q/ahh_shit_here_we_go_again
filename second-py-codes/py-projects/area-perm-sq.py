e0dfbff92a718ae50f0a238c24b3fe0a10d65487
Sahil002620Q <smax06388@gmail.com>
2026-01-22 23:26:27 +0530

    added new projects

---
+++ second-py-codes/py-projects/area-perm-sq.py
@@ -0,0 +1,3 @@
+n = int(input("enter side of square whose area you want to have: "))
+print("area of square is",n*n,"sq unit")
+print("perimeter of the square is",4*n,"unit")
--- second-py-codes/test.py
+++ second-py-codes/test.py
@@ -1,5 +1,7 @@
+arr = [1,2,3]
+arr2 = [2,3,4]
+print(arr+arr2)
 
-a = 2
-A = 'you can do it'
-print(a)
-print(A)
+input(arr = [[0]*3 for _ in range(3)])
+print(arr)
+
