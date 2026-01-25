6b4249878e74ea28177048d982ccea76abad3007
Sahil002620Q <smax06388@gmail.com>
2026-01-25 19:35:18 +0530

    temp comments

--- second-py-codes/py-projects/even-odd.py
+++ second-py-codes/py-projects/even-odd.py
@@ -1,9 +1,9 @@
 x = int(input("enter a number : "))
-if x==0:
-    print("num is not prime")
-else:
-    print("prime")
-
-
-
-#pending
+float = 29.0
+if x>2:
+    print("odd")
+elif x ==1:
+    print("even")
+#this line is for testing git temp
+# this one is also temp and will grt merge to main later
+#pending /explain
--- second-py-codes/test6.py
+++ second-py-codes/test6.py
@@ -1,1 +1,19 @@
 print('hello')
+
+# String Concatenation
+x = 'sudo'
+y = ' su'
+z = x + y
+print(z)
+
+
+# temp use 
+x =10
+y =101
+
+
+temp = x
+x = y 
+y = temp
+print(x, y)
+
