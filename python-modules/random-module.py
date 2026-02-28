f9c82463e200ad8bbe12308ec2ee38f60b235d0d
Sahil002620Q <smax06388@gmail.com>
2026-02-28 20:31:35 +0530

    python-modules

---
+++ python-modules/random-module.py
@@ -0,0 +1,8 @@
+import random 
+x = [1,2,3,4,5,6,7,8,9,10]  #list to be used for random choice , we will use random choice and i get random choice from this list 
+unknown = random.choice(x)   # create a variable which will store a random value from list
+print(unknown)  #print the random value takem by new variable
+
+# if we are importing something , the programme will search for the word we are importing like import
+# import random then the programme will search for "random" word and will look in folder too so if there 
+# will a file or folder named random it will target it so do not name a file with module name
---
+++ python-modules/time-module.py
@@ -0,0 +1,6 @@
+import time
+print('hello')
+time.sleep(0.5)  # just write this between two lines and add time in bracket, 
+print("the text will appear after 10 secoand") # it will sleep for that time and next line will be compiled after that time will gp
+time.sleep(10) 
+print('this is the text for which you are waiting')
